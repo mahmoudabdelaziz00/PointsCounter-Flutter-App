0279826db0123f645486a33a65c0a518190210e0
Mahmoud Abdelaziz <mahmoudabdelaziz00@gmail.com>
2025-05-18 17:10:19 +0300

    create teamIncrement method

--- lib/cubits/counter_cubit.dart
+++ lib/cubits/counter_cubit.dart
@@ -1,9 +1,19 @@
 import 'package:basketball/cubits/counter_state.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-class CounterCubit extends Cubit<CounterState>
-{
+class CounterCubit extends Cubit<CounterState> {
   CounterCubit(super.initialState);
 
+  int teamAPoints = 0;
+  int teamBPoints = 0;
 
+  void teamIncrement({required String team, required int buttonNumber}) {
+    if (team == 'A') {
+      teamAPoints += buttonNumber;
+      emit(CounterAIncrementState());
+    } else {
+      teamBPoints += buttonNumber;
+      emit(CounterBIncrementState());
+    }
+  }
 }
